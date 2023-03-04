81ed6b07fba38ce9639efe94fe7f3967a7206d0c
Ellen Bennett <ellenbennett@Ellens-MacBook-Air.local>
2023-03-03 19:56:36 -0700

    passing lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData (name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }, 
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com"
+        })
+    })
+    .then(resp => (resp.json()))
+    .then(function (object) {
+        let newId = object.id
+        document.querySelector('body').innerHTML = newId
+    })
+    .catch(function (message) {
+        document.querySelector('body').innerHTML = message
+    })
+};
